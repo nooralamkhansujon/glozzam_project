d48d8f90185eb4db9264c0ac1a8862459a589101
Nooralam khan <CoreyMShafer@gmail.com>
2020-02-07 19:51:00 +0600

    glozzam_project has been added

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+$('.slider').slick({
+    infinite: true,
+    slideToShow: 1,
+    slideToScroll: 1
+
+});
+
+// $('.slider').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   centerMode: true,
+//   variableWidth: true
+// });
